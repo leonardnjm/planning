632156db817b84585ec210e8ccd80dad36fc31b2
leonard <leonard.najman@gmail.com>
2020-05-29 17:37:40 +0200

    Planning MVC

---
+++ src/controleur/Controleur.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+import vue.Fenetre;
+
+/**
+ *
+ * Contrôle l'interrogation de la BDD dans la Fenetre
+ *
+ * @author segado
+ */
+public class Controleur {
+
+    /**
+     *
+     * une methode principal (main) pour lancer l'application
+     *
+     * @param s
+     */
+    public static void main(String[] s) {
+        // creation de la fenetre
+        Fenetre f = new Fenetre();
+    }
+}
